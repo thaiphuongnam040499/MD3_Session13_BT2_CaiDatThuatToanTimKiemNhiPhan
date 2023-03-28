60b80027e8e226e74ed99d55139ab93756471756
thaiphuongnam040499 <thainamvinhuni@gmail.com>
2023-03-28 09:34:20 +0700

    up

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,37 @@
-public class Main {
-    static int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
-    static int binarySearch(int[] list, int key) {
-        int low = 0;
-        int high = list.length - 1;
-        while (high >= low) {
-            int mid = (low + high) / 2;
-            if (key < list[mid])
-                high = mid - 1;
-            else if (key == list[mid])
-                return mid;
-            else
-                low = mid + 1;
-        }
-        return -1; /* Now high < low, key not found */
-    }
+import java.util.*;
+
+public class Main implements Comparator<Integer> {
     public static void main(String[] args) {
-        System.out.println(binarySearch(list, 2));  /* 0 */
-        System.out.println(binarySearch(list, 11)); /* 4 */
-        System.out.println(binarySearch(list, 79)); /*12 */
-        System.out.println(binarySearch(list, 1));  /*-1 */
-        System.out.println(binarySearch(list, 5));  /*-1 */
-        System.out.println(binarySearch(list, 80)); /*-1 */
+        Scanner input =new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            list.add(new Random().nextInt(20));
+        }
+        Collections.sort(list);
+        System.out.println(list);
+        System.out.println(binarySearch(list,0,list.size()-1,9));
 
     }
+    public  static int binarySearch(List<Integer> list,int left,int right,int value){
+        while (left <= right) {
+            int middle = (left + right)/2;
+            System.out.println(middle);
+            if(list.get(middle) == value){
+                return middle;
+            } else if (list.get(middle) > value) {
+                return binarySearch(list, left, middle-1, value);
+            }else {
+                return binarySearch(list, middle+1, right, value);
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o1-o2;
+    }
 }
+
